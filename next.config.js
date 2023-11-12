4d8911c3ce6e73d859394ed981307a1b8f0bbaf7
unimat <unimat@unimat.hu>
2023-11-12 14:47:26 +0100

    edit next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     API_URL: "https://bookit-v2.vercel.app",
     NEXTAUTH_URL: "https://bookit-v2.vercel.app",
     DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit-v2",
-    DB_URI: 
+    DB_URI: ""
   },
   images: {
     domains: ["res.cloudinary.com"],
